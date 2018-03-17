7ad411b681cf0caa482cb64d32084977531b4e9c
Phil Buendia <phil.buendia@gmail.com>
2018-03-16 21:49:47 -0700

    949

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
-/*$(document).ready(function(){
-
-		$(window).scroll(function() {
-			console.log("scrolling")
-			if ($(window).scrollTop() > 50) {
-				console.log("you scrolled too far");
-			}
-		});
-
-});*/
 
 var header = $('.header');
 
 $(window).scroll(function(e){
-    if(header.offset.top !== 0){
+   
+   if(header.offset.top !== 0){
         if(!header.hasClass('shadow')){
             $('body > div > div.header').addClass('shadow');
             console.log('with shadow');
         }
-    }else{
-    	$('body > div > div.header').removeClass('shadow');
-        header.removeClass('shadow');
-        console.log('shadow removed');
     }
 
-
    if($(window).scrollTop() === 0) {
-     $('.container > div:nth-child(1n)').removeClass('shadow');
+     $('body > div > div.header').removeClass('shadow');
      console.log('shadow removed');
    }
-
-
-
-
-
 
 });
 
